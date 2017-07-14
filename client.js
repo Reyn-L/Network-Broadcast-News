bbbc462829f6432501b46e6270afc8a8b7a21e2e
Reyn-L <Reyn.Leong@gmail.com>
2017-07-14 09:23:52 -1000

    username

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 /*jshint esversion: 6*/
 const net = require('net');
 const client = net.createConnection({port:6969}, () => {
-
+  process.stdout.write('Please enter your username' + '\n');
 });
 process.stdin.on('data', (data) => {
-  client.write(data.toString());
+  client.write(data);
+
 });
 
 client.on('data', (data) => {
--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 /*jshint esversion :6*/
 const net = require('net');
 var clients = [];
+var username = [];
 const server = net.createServer((socket) => {
-  process.stdout.write('connected');
+  process.stdout.write('\n' +'connected');
   clients.push(socket);
-  socket.on('close', () => {
-    process.stdout.write('client disconnected');
-  });
   socket.on('data', (data) => {
-    clients.forEach((client) => {
-      if(client !== socket){
-        client.write(data);
-     }
-   });
+    if(socket.hasOwnProperty('name')){
+     clients.forEach((user) => {
+      user.write(socket.name + data.toString());
+    });
+     return;
+   }
+   let avaliable = clients.every((user) => {
+    return data.toString() !== user.name;
   });
-
-
+   if(avaliable) {
+    socket.name = data.toString();
+  } else {
+    socket.write("Username is already taken. Please choose a new one." + '\n');
+  }
+});
+  socket.on('close', (client) => {
+    process.stdout.write('\n' + socket.name + ' disconnected');
+    // client.write('\n' + socket.username + " has disconnected");
+  });
+  // socket.on('data', (data) => {
+  //   clients.forEach((client) => {
+  //     if(client !== socket){
+  //       client.write("\n" + data);
+  //     } else if(data === socket.name) {
+  //       client.write('hi');
+  //     }
+  //   });
+  // });
 });
 server.listen(6969, "0.0.0.0", () => {
   process.stdout.write('listening for port 6969');
